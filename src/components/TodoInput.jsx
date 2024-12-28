6a468d055877f55b46b70eb27afa97b59445ce5e
yashkemkar <yashkemkar@gmail.com>
2024-12-28 19:17:05 +1100

    updated to submit on enter key press

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,10 +3,15 @@ export function TodoInput(props) {
     
     return (
         <div className="input-container">
-            <input type="text" id="input-text" value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} placeholder="Add Task" />
-            
-            <button onClick={() => {
-                if (!inputValue) {return} //Guard clause
+            <input type="text" id="input-text" value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} placeholder="Add Task" onKeyDown={(e) => {
+                if(e.key==="Enter") { //submit via enter key
+                    if (!inputValue) {return} //guard clause
+                    handleAddTodo(inputValue) 
+                    setInputValue("") 
+                }
+            }}/>          
+            <button onClick={() => { // submit via button
+                if (!inputValue) {return} //guard clause
                 handleAddTodo(inputValue) 
                 setInputValue("")
                 }}>
